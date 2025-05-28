a7044340da0cbd3e0e1cc9d79dec7c1632700f62
samuel tapia <samuel_macos@MacBook-Pro-de-samuel.local>
2025-05-27 21:28:14 -0400

    Agregando id a tabla usuarios

--- database/migrations/2025_05_27_073502_create_usuarios_table.php
+++ database/migrations/2025_05_27_073502_create_usuarios_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->string('rut')->primary();
+            $table->string('rut');
             $table->string('nombre');
             $table->string('apellido');
             $table->string('telefono');
